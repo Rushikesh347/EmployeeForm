c9b13b9d5c6a7281fbfc3e08fa0a37541f594dc7
Teleindia <Teleindia@DESKTOP-UKQ3UC8>
2023-04-24 17:34:49 +0530

    Add project files.

---
+++ ASPNETMVCCRUD/Program.cs
@@ -0,0 +1,34 @@
+using ASPNETMVCCRUD.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCDemoDbContext>(Options =>
+    Options.UseSqlServer(builder.Configuration
+    .GetConnectionString("MvcDemoConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
